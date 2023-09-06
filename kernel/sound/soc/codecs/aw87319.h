d01c190ccd36cb48fee2c5bb62bf443333a9763f
vmarih <ajenkaxoyk@gmail.com>
2023-09-06 22:15:22 +0700

    upload

---
+++ kernel/sound/soc/codecs/aw87319.h
@@ -0,0 +1,11 @@
+/*
+ *aw87319.h  --  AW87319 Soc Audio driver
+ */
+
+#ifndef _AW89319_H
+#define _AW89319_H
+
+extern unsigned char AW87319_Audio_Speaker(void);
+extern unsigned char AW87319_Audio_OFF(void);
+
+#endif
